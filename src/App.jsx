2d153d39736d80bb3209ce94865cfef2ba08e4b4
evgenia-fienik <160489180+evgenia-fienik@users.noreply.github.com>
2024-10-24 13:42:19 +0300

    +-

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,10 @@
-import { useState } from 'react'
-import FeedbackForm from './FeedbackForm/FeedbackForm'
-import MyComponent from './MyComponent';
-import SearchBar from './SearchBar';
 import './App.css'
 
+
 function App() {
-
-
-  const [count, setCount] = useState(0)
-
-
-  const handleLogin = (loginData) => {
-    console.log('User lpgin data:', loginData);
-  };
-
-  const handleChange = (evt) => {
-    setInputValue(evt.target.value);
-  };
-
   return (
     <div>
-      <h1>Please login to your account!</h1>
-      {/* Передаємо колбек як пропс в компонент LoginForm */}
-      <FeedbackForm />
-      <MyComponent />
-      <SearchBar/>
-      
+
     </div>
   )
   
